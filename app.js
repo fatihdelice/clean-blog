3076d58ddb48345bf5b0155f07aeb32037f3352e
Fatih Delice <delicex58@gmail.com>
2022-12-27 02:15:52 +0300

    update - delete added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
+const methodOverride = require('method-override');
 const path = require('path');
 const Post = require('./models/Post');
+const postController = require('./controllers/postControllers');
+const pageController = require('./controllers/pageControllers');
 
 const app = express();
 
@@ -17,35 +20,22 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 //ROUTES
-app.get('/', async (req, res) => {
-  const posts = await Post.find({});
-  res.render('index', {
-    posts: posts,
-  });
-});
+app.get('/', postController.getAllPosts);
+app.get('/posts/:id', postController.getPost);
+app.post('/posts', postController.getCreatePost);
+app.put('/posts/:id', postController.updatePost);
+app.delete('/posts/:id', postController.deletePost);
 
-app.get('/posts/:id', async (req, res) => {
-  //console.log(req.params.id);
-  const post = await Post.findById(req.params.id);
-  res.render('post', {
-    post: post,
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/add', (req, res) => {
-  res.render('add_post');
-});
-
-app.post('/posts', async (req, res) => {
-  await Post.create(req.body);
-  res.redirect('/');
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPostPage);
+app.get('/posts/edit/:id', pageController.getEditPage);
 
 const port = 5000;
 app.listen(port, () => {
